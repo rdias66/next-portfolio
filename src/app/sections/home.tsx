6887cc9ecb0eecebe822403852deba0a7dba73e2
rdias66 <dias.a@live.com>
2024-05-16 17:55:04 -0300

    home elements positions kinda structured

--- src/app/sections/home.tsx
+++ src/app/sections/home.tsx
@@ -1,13 +1,23 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import DevGif from '@/app/assets/dev-gif.gif'
 
 export function Home({ LanguageTextData }: any) {
   return (
-    <section id="Home" className="flex h-5/6">
-      <div className="w-1/2 bg-slate-700 p-10 m-auto flex-row">
-        <h1 className="text-4xl">{LanguageTextData.mainText}</h1>
-        <p className="text-xl">{LanguageTextData.introduction} </p>
-
-        <Button className="">{LanguageTextData.buttonText}</Button>
+    <section id="Home">
+      <div className="container mx-auto px-4 bg-slate-700">
+        <div className="flex flex-wrap -mx-2">
+          <div className="w-full md:w-1/2 px-2">
+            <span className="text-4xl  max-w-full">
+              {LanguageTextData.mainText}
+            </span>
+            <p className="text-xl ">{LanguageTextData.introduction} </p>
+            <Button className="">{LanguageTextData.buttonText}</Button>
+          </div>
+          <div className="w-full md:w-1/2 px-2">
+            <Image src={DevGif} alt="Dev Gif" />
+          </div>
+        </div>
       </div>
     </section>
   )
