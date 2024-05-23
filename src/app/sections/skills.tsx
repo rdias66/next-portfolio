c00a157497a5cfe1aa9f24428b5c2cdf7a702512
rdias66 <dias.a@live.com>
2024-05-23 12:54:50 -0300

    styling done on skills, need content

--- src/app/sections/skills.tsx
+++ src/app/sections/skills.tsx
@@ -3,12 +3,15 @@ import Image from 'next/image'
 
 export function Skills({ LanguageTextData }: any) {
   return (
-    <section id="Skills">
-      <div className="mt-40  w-4/5 rounded-2xl mx-auto flex flex-wrap">
+    <section
+      id="Skills"
+      className="flex items-center justify-center min-h-screen mt-40"
+    >
+      <div className="w-4/5 rounded-2xl flex flex-wrap justify-center items-center">
         {iconCards.map((card, index) => (
           <div
             key={index}
-            className={`flex flex-col items-center justify-center w-1/6 h-3/6 m-5 p-3 ${card.boxStyle}`}
+            className={`flex flex-col items-center justify-center w-1/5 h-5/6 m-5 p-3 ${card.boxStyle}`}
           >
             <Image src={card.icon} alt={card.name} width={40} height={40} />
             <span className="text-white mt-2">{card.name}</span>
