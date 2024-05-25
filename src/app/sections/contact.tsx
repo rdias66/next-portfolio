7f12bea2e1b12631cd707d2c27cc08fe67531873
rdias66 <dias.a@live.com>
2024-05-25 13:18:51 -0300

    bottom text and socials added

--- src/app/sections/contact.tsx
+++ src/app/sections/contact.tsx
@@ -7,6 +7,23 @@ export function Contact({ LanguageTextData }: any) {
         <h1>{LanguageTextData.title}</h1>
         <p>{LanguageTextData.contactText}</p>
         <Button>{LanguageTextData.buttonText}</Button>
+        <a
+          href="https://www.instagram.com/diasrodr"
+          className="hover:text-zinc-500"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-instagram fa-xl"></i>
+        </a>
+        <a
+          href="https://www.twitter.com/1deltakream"
+          className="hover:text-zinc-500"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-twitter fa-xl"></i>
+        </a>
+        <h3>{LanguageTextData.bottomContactText}</h3>
       </div>
     </section>
   )
--- src/components/ui/socials.tsx
+++ src/components/ui/socials.tsx
@@ -18,14 +18,6 @@ export function Socials() {
       >
         <i className="fa-brands fa-linkedin fa-xl"></i>
       </a>
-      <a
-        href="https://www.instagram.com/diasrodr"
-        className="hover:text-zinc-500"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa-brands fa-instagram fa-xl"></i>
-      </a>
     </div>
   )
 }
