9bf99dee03be158d066445fbd57080b362fc1eea
rdias66 <dias.a@live.com>
2024-05-25 12:29:19 -0300

    contact initial struct

--- src/app/sections/contact.tsx
+++ src/app/sections/contact.tsx
@@ -1,7 +1,13 @@
-export function Contact() {
+import { Button } from '@/components/ui/button'
+
+export function Contact({ LanguageTextData }: any) {
   return (
-    <section id="Contact">
-      <h1>This is contact</h1>
+    <section id="Contact" className="mt-64">
+      <div>
+        <h1>{LanguageTextData.title}</h1>
+        <p>{LanguageTextData.contactText}</p>
+        <Button>{LanguageTextData.buttonText}</Button>
+      </div>
     </section>
   )
 }
