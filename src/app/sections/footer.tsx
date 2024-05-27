0d5d09151d926f798cdefd5671a4ab7d70b725f3
rdias66 <dias.a@live.com>
2024-05-27 16:09:59 -0300

    footer init struct done

--- src/app/sections/footer.tsx
+++ src/app/sections/footer.tsx
@@ -1,3 +1,21 @@
+import { Separator } from '@/components/ui/separator'
+
 export function Footer() {
-  return <h1>This is footer</h1>
+  return (
+    <div className="mt-10 static bottom-0 w-full h-20  bg-zinc-800 bg-opacity-60 text-white ">
+      <Separator className="w-full" orientation="horizontal" />
+      <div className="flex flex-row justify-evenly items-center mt-7">
+        <h3 className=" my-auto">Built with</h3>
+        <span className="text-sm font-thin  my-auto ">2024</span>
+        <a
+          href="https://www.github.com/rdias66/next-portfolio"
+          className="hover:underline hover:underline-offset-2 hover:text-zinc-400  my-auto "
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className="text-sm font-thin">Source code</p>
+        </a>
+      </div>
+    </div>
+  )
 }
