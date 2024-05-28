038836588e1e8b48a4d8191e2ef0c3f3a93161d4
rdias66 <dias.a@live.com>
2024-05-28 12:08:35 -0300

    hover card added

--- src/app/sections/footer.tsx
+++ src/app/sections/footer.tsx
@@ -1,11 +1,56 @@
 import { Separator } from '@/components/ui/separator'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export function Footer() {
   return (
     <div className="mt-10 static bottom-0 w-full h-20  bg-zinc-800 bg-opacity-60 text-white ">
       <Separator className="w-full" orientation="horizontal" />
       <div className="flex flex-row justify-evenly items-center mt-7">
-        <h3 className=" my-auto">Built with</h3>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <h4 className="text-white text-sm cursor-default">Built with</h4>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80 bg-zinc-900 text-white ">
+            <div className="flex flex-col ">
+              <div className="flex flex-row p-3">
+                <Avatar>
+                  <AvatarImage src="https://github.com/vercel.png" />
+                  <AvatarFallback>VC</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1 p-2">
+                  <h4 className="text-sm font-semibold">@nextjs</h4>
+                  <p className="text-sm">React Framework .</p>
+                </div>
+              </div>
+              <div className="flex flex-row p-3">
+                <Avatar>
+                  <AvatarImage src="https://github.com/tailwindcss.png" />
+                  <AvatarFallback>TW</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1 p-2">
+                  <h4 className="text-sm font-semibold">@tailwindcss</h4>
+                  <p className="text-sm">CSS Framework.</p>
+                </div>
+              </div>
+              <div className="flex flex-row p-3">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn-ui.png" />
+                  <AvatarFallback>SH</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1 p-2">
+                  <h4 className="text-sm font-semibold">@shadcn-ui</h4>
+                  <p className="text-sm">UI Framework.</p>
+                </div>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+
         <span className="text-sm font-thin  my-auto ">2024</span>
         <a
           href="https://www.github.com/rdias66/next-portfolio"
